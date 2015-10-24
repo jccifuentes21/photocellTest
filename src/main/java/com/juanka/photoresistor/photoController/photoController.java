0f09bb4f021282d5b5211368d9ca8c88105be8a7
jccifuentes21 <jccifuentes21@gmail.com>
2015-10-24 10:33:08 -0500

    Fist Commit

---
+++ src/main/java/com/juanka/photoresistor/photoController/photoController.java
@@ -0,0 +1,28 @@
+package com.juanka.photoresistor.photoController;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinAnalogInput;
+import com.pi4j.io.gpio.RaspiPin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class photoController {
+
+    public String start() {
+        return "PhotoResistor test started";
+    }
+
+    @RequestMapping("/test")
+    public static GpioPinAnalogInput test (){
+
+        GpioController gpio = GpioFactory.getInstance();
+
+        GpioPinAnalogInput photoresistor = gpio.provisionAnalogInputPin(RaspiPin.GPIO_00, "Fotoresistencia");
+
+
+        return (photoresistor);
+    }
+
+}
