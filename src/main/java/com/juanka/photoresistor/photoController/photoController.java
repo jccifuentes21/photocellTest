b0a049238c12ef6b465df11e70caa0fb2b4df4d1
jccifuentes21 <jccifuentes21@gmail.com>
2015-10-24 11:35:43 -0500

    4th commit

--- src/main/java/com/juanka/photoresistor/photoController/photoController.java
+++ src/main/java/com/juanka/photoresistor/photoController/photoController.java
@@ -1,25 +1,23 @@
 package com.juanka.photoresistor.photoController;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinAnalogInput;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class photoController {
 
+    @RequestMapping("/")
     public String start() {
         return "PhotoResistor test started";
     }
 
     @RequestMapping("/test")
-    public static GpioPinAnalogInput test (){
+    public static GpioPinDigitalInput test (){
 
         GpioController gpio = GpioFactory.getInstance();
 
-        GpioPinAnalogInput photoresistor = gpio.provisionAnalogInputPin(RaspiPin.GPIO_01, "Fotoresistencia");
+        GpioPinDigitalInput photoresistor = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, "Fotoresistencia");
 
 
         return (photoresistor);
